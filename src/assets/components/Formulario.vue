b5ee727033095398c25913e4ea4f00e0629e7641
liluriel28 <a3521110073@alumno.uttehuacan.edu.mx>
2022-10-05 23:44:39 -0500

    Segundo cambio

--- src/assets/components/Formulario.vue
+++ src/assets/components/Formulario.vue
@@ -1,58 +1,89 @@
 <template>
 
-    <form @submit.prevent="registrarProyecto">
-      <div class="mb-3">
-        <label  class="form-label">Proyecto</label>
-        <input v-model.trim="proyecto" type="text" class="form-control" required />
-      </div>
+    <div class="row">
+        <div class="col-12 mb-4">
+            <h3 class="text-center" > Proceso: 0%  </h3>
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" 
+                role="progressbar" 
+                aria-valuenow="25" 
+                aria-valuemin="0" 
+                aria-valuemax="100"
+                style="width: 25%"
+                > </div>
+            </div>
+        </div>
 
-      <div class="mb-3">
-        <label  class="form-label">Actividad</label>
-        <select v-model="tipo" class="form-select" required>
-            <option disabled selected value="">Selecciona un tipo de actividad</option>
-            <option>Aplicaciones Wen con Vue.js</option>
-            <option>Backend Services con Node.js</option>
-            <option>App móvil con React Native</option>
-        </select>
-      </div>
+        <div class="col-12 col-md-4">
+            <form @submit.prevent="registrarProyecto">
+                <div class="mb-3">
+                    <label  class="form-label">Proyecto</label>
+                    <input v-model.trim="proyecto" type="text" class="form-control" required />
+                </div>
 
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-check-label">Urgente</label>
-        <input v-model="urgente" type="checkbox" class="form-check-input"/>
-      </div>
+                <div class="mb-3">
+                    <label  class="form-label">Actividad</label>
+                    <select v-model="tipo" class="form-select" required>
+                        <option disabled selected value="">Selecciona un tipo de actividad</option>
+                        <option>Aplicaciones Wen con Vue.js</option>
+                        <option>Backend Services con Node.js</option>
+                        <option>App móvil con React Native</option>
+                    </select>
+                </div>
+
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-check-label">Urgente</label>
+                    <input v-model="urgente" type="checkbox" class="form-check-input"/>
+                </div>
 
 
-      <button type="submit" class="btn btn-primary">Guardar</button>
-    </form>
-    <hr>
-    <!-- <pre>
-        {{ proyecto }}
-        {{ tipo }}
-        {{ urgente }}
-    </pre> -->
+            <button type="submit" class="btn btn-primary">Guardar</button>
+            </form>  
+        </div>
+        <div class="col-12 col-md-8">
+            <hr>
+            <h3>
+                <!-- propiedades calculadas| computadas -->
+                Total Proyectos: {{ numeroProyectos }}
+            </h3>
+            <!-- <pre>
+                {{ proyecto }}
+                {{ tipo }}
+                {{ urgente }}
+            </pre> -->
 
-    <div class="table-responsive">
-        <table class="table table-dark">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Proyecto</th>
-                    <th>Tipo</th>
-                    <th>Urgente</th>
-                </tr>
-            </thead>
+            <div class="table-responsive">
+                <!-- el over nos sirve para pintar -->
+                <table class="table table-dark table-hover">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Proyecto</th>
+                            <th>Tipo</th>
+                            <th>Urgente</th>
+                            <th>Completado</th>
+                        </tr>
+                    </thead>
 
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Lavarel app</td>
-                    <td>Lavarel</td>
-                    <td class="bg-success">SI | NO</td>
-                </tr>
-            </tbody>
-        </table>
+                    <tbody>
+                        <tr v-for="(proyecto, index) in proyectos" :key="index">
+                            <td>{{ index+1 }}</td>
+                            <td> {{ proyecto.proyecto }} </td>
+                            <td> {{ proyecto.tipo }} </td>
+                            <!-- operador ternario -->
+                            <td @click="cambiarEstado(proyecto, 'urgente')" :class="proyecto.urgente ? 'bg-success' : 'bg-danger'"> {{ proyecto.urgente ? "Si" : "No" }} </td>
+                            <td @click="cambiarEstado(proyecto, 'completado')" :class="proyecto.completado ? 'bg-success' : 'bg-danger'"> {{ proyecto.completado ? "Completo" : "Incompleto" }} </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+
+        </div>
     </div>
 
+    
+    
 </template>
 
 <script>
@@ -62,6 +93,7 @@
             tipo: "",
             urgente: false,
             proyectos: [],
+            
         }),
         methods:{
             registrarProyecto(){
@@ -69,12 +101,31 @@
                     proyecto: this.proyecto,
                     tipo: this.tipo,
                     urgente: this.urgente,
+                    completado: false,
                 };
                 this.proyectos.push(proyecto);
+
+                
+
                 this.proyecto = "";
                 this.tipo = "";
                 this.urgente = false;
             },
+
+            cambiarEstado(proyecto, campo){
+                // el signo de admiracion lo cambia
+                // this.proyectos[id].urgente = !this.proyectos[id].urgente;
+                // console.log(proyecto, campo);
+                proyecto[campo] = !proyecto[campo];
+            }
+
+        },
+        // propiedades computadas
+        computed:{
+            numeroProyectos(){
+                return this.proyectos.length;
+            },
         },
     };
 </script>
+
